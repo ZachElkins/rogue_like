d8665be9936745cf87444786dd15fab0d6753c05
Zelkins12 <zach.elkins@gmail.com>
2020-03-29 20:54:56 -0400

    cleaned up a bit

--- level.py
+++ level.py
@@ -2,7 +2,7 @@
 from lib import *
 
 class Level:
-    def __init__(self, diff):
+    def __init__( self, diff ) :
         # Size of the level
         self.size = diff
         # Difficulty of the level
@@ -13,15 +13,15 @@ class Level:
         self.rooms = []
         self.generate()
 
-    def generate(self):
+    def generate( self ) :
 
         # Reset rooms
         self.rooms = []
 
-        print(f'generating a new map (difficulty: {self.diff})')
+        print( f'generating a new map (difficulty: {self.diff})' )
 
         # Create first room
-        self.rooms.append( (random.randint(0, self.size-1), random.randint(0, self.size-1)) )
+        self.rooms.append( ( random.randint( 0, self.size-1 ), random.randint( 0, self.size-1 ) ) )
         
         i = 0
         while i < self.diff:
@@ -31,31 +31,31 @@ class Level:
             for room in self.rooms:
                 # Left
                 if room[0] != 0:
-                    room_options.append( (room[0]-1, room[1]) )
+                    room_options.append( ( room[0]-1, room[1] ) )
                 # Right
                 if room[0] < self.size-1:
-                    room_options.append( (room[0]+1, room[1]) )
+                    room_options.append( ( room[0]+1, room[1] ) )
                 # Top
                 if room[1] != 0:
-                    room_options.append( (room[0], room[1]-1) )
+                    room_options.append( ( room[0], room[1]-1 ) )
                 # Bottom
                 if room[1] < self.size-1:
-                    room_options.append( (room[0], room[1]+1) )
+                    room_options.append( ( room[0], room[1]+1 ) )
 
                 # Randomly choose a room
-                new_room = room_options[random.randint(0, len(room_options)-1)]
+                new_room = room_options[random.randint( 0, len( room_options )-1 )]
 
             # Check if new room is a duplicates
             if not new_room in self.rooms:
-                self.rooms.append(new_room)
+                self.rooms.append( new_room )
                 self.map[room[0]][room[1]] = '[#]'
                 i += 1
 
-    def get_map(self):
+    def get_map( self ) :
         return self.map
 
-    def get_rooms(self):
+    def get_rooms( self ) :
         return self.size
     
-    def get_size(self):
+    def get_size( self ) :
         return self.size
--- main.py
+++ main.py
@@ -2,10 +2,10 @@
 from lib import *
 from level import *
 
-def main():
-    print("hello world")
-    level = Level(10)
-    print(np.matrix(level.get_map()))
+def main() :
+    print( "hello world" )
+    level = Level( 10 )
+    print( np.matrix( level.map() ) )
 
 if __name__ == '__main__':
     main()
