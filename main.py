9d95f42b348142fa4a8405e2f452ae91cd6185c1
Zelkins12 <zach.elkins@gmail.com>
2020-03-31 17:45:46 -0400

    added basic pygame functionality

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python3
 from lib import *
 from level import *
+import pygame, sys
+from pygame.locals import *
+
+def start_game():
+    pygame.init()
+    DISPALYSURF = pygame.display.set_mode( ( 750, 750 ) )
+    pygame.display.set_caption( "rouge_like" )
+    while True :
+        for event in pygame.event.get() :
+            print(event)
+            if event.type == QUIT :
+                pygame.quit()
+                sys.exit()
 
 def main() :
     level = Level( 10 )
     print( np.matrix( level.get_map() ) )
+    start_game()
 
 if __name__ == '__main__' :
     main()
