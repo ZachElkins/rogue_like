dd0a756d274b00d0f2572d4feff85a7065822cfc
Zelkins12 <zach.elkins@gmail.com>
2020-04-14 21:59:39 -0400

    Prepared new sprites

--- sprites.py
+++ sprites.py
@@ -7,6 +7,20 @@ PLAYER_SPRITES = {
     "R" : pygame.image.load( "./sprites/player/player_right.png" )
 }
 
+SKELETON_SPRITES = {
+    "U" : pygame.image.load( "./sprites/enemies/skeleton/skeleton_up.png" ),
+    "D" : pygame.image.load( "./sprites/enemies/skeleton/skeleton_down.png" ),
+    "L" : pygame.image.load( "./sprites/enemies/skeleton/skeleton_left.png" ),
+    "R" : pygame.image.load( "./sprites/enemies/skeleton/skeleton_right.png" )
+}
+
+ITEM_SPRITES = {
+    "key"   : pygame.image.load( "./sprites/pickups/key.png" ),
+    "potion": pygame.image.load( "./sprites/pickups/potion.png" ),
+    "coin"  : pygame.image.load( "./sprites/pickups/coin.png" ),
+    "bomb"  : pygame.image.load( "./sprites/pickups/bomb.png" ),
+}
+
 FLOOR_SPRITES = []
 FLOOR_SPRITES.extend( repeat( pygame.image.load( "./sprites/tiles/floor/floor_01.png" ), 2 ) )
 FLOOR_SPRITES.extend( repeat( pygame.image.load( "./sprites/tiles/floor/floor_02.png" ), 2 ) )
